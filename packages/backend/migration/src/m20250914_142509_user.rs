892d650eacf03ecd2be475d8958aee3d89be73ee
StelStelox <stelstelox@gmail.com>
2025-09-14 22:06:12 +0300

    started axum main function

--- packages/backend/migration/src/m20250914_142509_user.rs
+++ packages/backend/migration/src/m20250914_142509_user.rs
@@ -52,7 +52,7 @@ impl MigrationTrait for Migration {
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
         // Replace the sample below with your own migration scripts
-        todo!();
+        // todo!();
 
         manager
             .drop_table(Table::drop().table(User::Table).to_owned())
