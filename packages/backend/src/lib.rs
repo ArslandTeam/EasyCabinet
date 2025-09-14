c9b3fda856e34569aa4faf10ca4a15572f100876
StelStelox <stelstelox@gmail.com>
2025-09-14 23:41:17 +0300

    call only function in main.rs

---
+++ packages/backend/src/lib.rs
@@ -0,0 +1,16 @@
+use std::env;
+
+use axum::{Json, Router, routing::get};
+use serde_json::json;
+
+#[tokio::main]
+pub async fn start_backend() {
+    dotenvy::dotenv().ok();
+    let app = Router::new().route("/", get(Json(json!({"status": "ok"}))));
+    let host = env::var("HOST").expect("HOST key not set in .env");
+    let port = env::var("PORT").expect("PORT key not set in .env");
+    let listener = tokio::net::TcpListener::bind(format!("{host}:{port}"))
+        .await
+        .unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
--- packages/backend/src/main.rs
+++ packages/backend/src/main.rs
@@ -1,16 +1,3 @@
-use std::env;
-
-use axum::{Json, Router, routing::get};
-use serde_json::json;
-
-#[tokio::main]
-async fn main() {
-    dotenvy::dotenv().ok();
-    let app = Router::new().route("/", get(Json(json!({"status": "ok"}))));
-    let host = env::var("HOST").expect("HOST key not set in .env");
-    let port = env::var("PORT").expect("PORT key not set in .env");
-    let listener = tokio::net::TcpListener::bind(format!("{host}:{port}"))
-        .await
-        .unwrap();
-    axum::serve(listener, app).await.unwrap();
+fn main() {
+    backend::start_backend();
 }
