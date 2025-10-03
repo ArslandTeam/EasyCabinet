2dabbfd53a238a32e516bdb8308cb644acc87feb
StelStelox <stelstelox@gmail.com>
2025-10-03 11:28:40 +0300

    Add log, entity user and update service

--- packages/backend/src/auth/controller.rs
+++ packages/backend/src/auth/controller.rs
@@ -1,11 +1,15 @@
-use axum::{Json, http::StatusCode, response::IntoResponse};
-
 use crate::{
-    BackendError,
+    AppState, BackendError,
     auth::dto::{LoginDTO, RegisterDTO},
+    auth::service,
 };
+use axum::{Json, extract::State, response::IntoResponse};
 
-pub async fn login(Json(payload): Json<LoginDTO>) -> Result<impl IntoResponse, BackendError> {
+pub async fn login(
+    State(state): State<AppState>,
+    Json(payload): Json<LoginDTO>,
+) -> Result<impl IntoResponse, BackendError> {
+    service::login(&state.conn, payload).await?;
     Ok(())
 }
 
--- packages/backend/src/auth/service.rs
+++ packages/backend/src/auth/service.rs
@@ -1,26 +1,35 @@
-use axum::response::IntoResponse;
+use crate::{BackendError, auth::dto::LoginDTO, entities::users, user::service};
+use bcrypt::{BcryptError, hash, verify};
 use sea_orm::DatabaseConnection;
-
-use crate::{BackendError, auth::dto::LoginDTO, user::service};
 
 pub async fn verify_auth(
     db: &DatabaseConnection,
     login: String,
     password: String,
-) -> Result<impl IntoResponse, BackendError> {
+) -> Result<users::Model, BackendError> {
     let user = service::find_user(&db, login)
         .await
         .map_err(|_| BackendError::InternalError)?;
 
     match user {
-        Some(_) => Ok(()),
+        Some(user) => {
+            check_password(password, &user.password)
+                .map_err(|_| BackendError::BadRequest("Invalid password".to_string()))?;
+            Ok(user)
+        }
         None => Err(BackendError::BadRequest("User not found".to_string())),
     }
 }
 
-pub async fn login(
-    db: &DatabaseConnection,
-    data: LoginDTO,
-) -> Result<impl IntoResponse, BackendError> {
-    verify_auth(db, data.login, data.password).await
+pub async fn login(db: &DatabaseConnection, data: LoginDTO) -> Result<(), BackendError> {
+    verify_auth(db, data.login, data.password).await?;
+    Ok(())
+}
+
+fn generate_hash_password(password: String) -> String {
+    hash(password, 10).unwrap()
+}
+
+fn check_password(password: String, hash: &str) -> Result<bool, BcryptError> {
+    verify(password, hash)
 }
