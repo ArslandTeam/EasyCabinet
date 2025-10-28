c5cf5fb03463db623123b0359327e2ef3b945b34
StelStelox <stelstelox@gmail.com>
2025-10-28 21:59:39 +0300

    remove serde rename remove in aurora dto

--- packages/backend/src/api/aurora/dto.rs
+++ packages/backend/src/api/aurora/dto.rs
@@ -2,24 +2,19 @@ use serde::Deserialize;
 
 #[derive(Deserialize)]
 pub struct RequestJoinDto {
-    #[serde(rename = "accessToken")]
     pub access_token: String,
-    #[serde(rename = "userUUID")]
     pub user_uuid: String,
-    #[serde(rename = "serverID")]
     pub server_id: String,
 }
 
 #[derive(Deserialize)]
 pub struct RequestHasJoinedDto {
     pub username: String,
-    #[serde(rename = "serverID")]
     pub server_id: String,
 }
 
 #[derive(Deserialize)]
 pub struct RequestProfileDTO {
-    #[serde(rename = "userUUID")]
     pub user_uuid: String,
 }
 
