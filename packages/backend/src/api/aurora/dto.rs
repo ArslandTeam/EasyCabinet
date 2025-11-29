45396b25236902d45f3439964492485a724ab08d
StelStelox <stelstelox@gmail.com>
2025-11-29 14:03:41 +0300

    Fix DTO aurora

--- packages/backend/src/api/aurora/dto.rs
+++ packages/backend/src/api/aurora/dto.rs
@@ -1,6 +1,7 @@
 use serde::Deserialize;
 
 #[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
 pub struct RequestJoinDto {
     pub access_token: String,
     pub user_uuid: String,
@@ -8,12 +9,14 @@ pub struct RequestJoinDto {
 }
 
 #[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
 pub struct RequestHasJoinedDto {
     pub username: String,
     pub server_id: String,
 }
 
 #[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
 pub struct RequestProfileDTO {
     pub user_uuid: String,
 }
