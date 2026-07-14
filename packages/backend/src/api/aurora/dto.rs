a91ef26db6250a33e228805db4a6950a401b3a44
StelStelox <stelstelox@arsland.ru>
2026-07-14 05:11:04 +0300

    UPD: add payload reposnse aurora service

--- packages/backend/src/api/aurora/dto.rs
+++ packages/backend/src/api/aurora/dto.rs
@@ -1,4 +1,4 @@
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize)]
 pub struct RequestJoinDto {
@@ -26,4 +26,47 @@ pub struct RequestProfileDTO {
 #[derive(Deserialize)]
 pub struct RequestProfilesDto {
     pub usernames: Vec<String>,
+}
+
+#[derive(Serialize)]
+pub struct AuroraResponse<T> {
+    pub success: bool,
+    pub result: T,
+}
+
+#[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct AuthResponseDto {
+    pub username: String,
+    #[serde(rename = "userUUID")]
+    pub user_uuid: String,
+    pub access_token: String,
+    pub is_alex: Option<bool>,
+    pub skin_url: Option<String>,
+    pub cape_url: Option<String>,
+}
+
+#[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct HasJoinResponseDto {
+    #[serde(rename = "userUUID")]
+    pub user_uuid: String,
+    pub is_alex: Option<bool>,
+    pub skin_url: Option<String>,
+    pub cape_url: Option<String>,
+}
+
+#[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ProfileResponseDto {
+    pub username: String,
+    pub is_alex: Option<bool>,
+    pub skin_url: Option<String>,
+    pub cape_url: Option<String>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct ProfilesResponseDto {
+    pub id: String,
+    pub name: String,
 }
--- packages/backend/src/api/aurora/service.rs
+++ packages/backend/src/api/aurora/service.rs
@@ -1,7 +1,13 @@
 use crate::{
     BackendError,
     api::{
-        aurora,
+        aurora::{
+            self,
+            dto::{
+                AuroraResponse, AuthResponseDto, HasJoinResponseDto, ProfileResponseDto,
+                ProfilesResponseDto,
+            },
+        },
         auth::service::AuthService,
         database::{entities::users, service::DatabaseService},
         storage_manager::StorageService,
@@ -10,7 +16,6 @@ use crate::{
 };
 use axum::Json;
 use sea_orm::DatabaseConnection;
-use serde_json::{Value, json};
 
 pub struct AuroraService;
 
@@ -20,7 +25,7 @@ impl AuroraService {
         storage: &StorageService,
         login: String,
         password: String,
-    ) -> Result<Json<Value>, BackendError> {
+    ) -> Result<Json<AuroraResponse<AuthResponseDto>>, BackendError> {
         let user = AuthService::verify_auth(db, &login, password)
             .await
             .map_err(|_| BackendError::BadRequestAurora("Incorecrt password or login".into()))?;
@@ -39,38 +44,29 @@ impl AuroraService {
 
         let textures = UserService::get_textures_data(storage, &user).await;
 
-        Ok(Json(json!({
-            "success": true,
-            "result": {
-                "username": user.login,
-                "userUUID": user.uuid,
-                "accessToken": access_token,
-                "isAlex": textures.is_alex,
-                "skinUrl": textures.skin_url,
-                "capeUrl": textures.cape_url,
-            }
-        })))
+        Ok(AuroraResponse::ok(AuthResponseDto {
+            username: user.login,
+            user_uuid: user.uuid,
+            access_token,
+            is_alex: textures.is_alex,
+            skin_url: textures.skin_url,
+            cape_url: textures.cape_url,
+        }))
     }
 
     pub async fn join(
         db: &DatabaseConnection,
         body: aurora::dto::RequestJoinDto,
-    ) -> Result<Json<Value>, BackendError> {
+    ) -> Result<Json<AuroraResponse<bool>>, BackendError> {
         let Some(user) = DatabaseService::find_user(db, users::Column::Uuid, &body.user_uuid)
             .await
             .map_err(|_| BackendError::InternalError)?
         else {
-            return Ok(Json(json!({
-                "success": true,
-                "result": false
-            })));
+            return Ok(AuroraResponse::ok(false));
         };
 
         if user.access_token != Some(body.access_token) {
-            return Ok(Json(json!({
-                "success": true,
-                "result": false
-            })));
+            return Ok(AuroraResponse::ok(true));
         }
 
         DatabaseService::update_user(
@@ -83,17 +79,14 @@ impl AuroraService {
         .await
         .map_err(|_| BackendError::InternalError)?;
 
-        Ok(Json(json!({
-            "success": true,
-            "result": true
-        })))
+        Ok(AuroraResponse::ok(true))
     }
 
     pub async fn has_join(
         db: &DatabaseConnection,
         storage: &StorageService,
         body: aurora::dto::RequestHasJoinedDto,
-    ) -> Result<Json<Value>, BackendError> {
+    ) -> Result<Json<AuroraResponse<HasJoinResponseDto>>, BackendError> {
         let user = DatabaseService::find_user(db, users::Column::Login, &body.username)
             .await
             .map_err(|_| BackendError::InternalError)?
@@ -105,22 +98,19 @@ impl AuroraService {
 
         let textures = UserService::get_textures_data(storage, &user).await;
 
-        Ok(Json(json!({
-            "success": true,
-            "result": {
-                "userUUID": user.uuid,
-                "isAlex": textures.is_alex,
-                "skinUrl": textures.skin_url,
-                "capeUrl": textures.cape_url,
-            }
-        })))
+        Ok(AuroraResponse::ok(HasJoinResponseDto {
+            user_uuid: user.uuid,
+            is_alex: textures.is_alex,
+            skin_url: textures.skin_url,
+            cape_url: textures.cape_url,
+        }))
     }
 
     pub async fn profile(
         db: &DatabaseConnection,
         storage: &StorageService,
         body: aurora::dto::RequestProfileDTO,
-    ) -> Result<Json<Value>, BackendError> {
+    ) -> Result<Json<AuroraResponse<ProfileResponseDto>>, BackendError> {
         let user = DatabaseService::find_user(db, users::Column::Uuid, &body.user_uuid)
             .await
             .map_err(|_| BackendError::InternalError)?
@@ -128,35 +118,39 @@ impl AuroraService {
 
         let textures = UserService::get_textures_data(storage, &user).await;
 
-        Ok(Json(json!({
-            "success": true,
-            "result": {
-                "username": user.login,
-                "isAlex": textures.is_alex,
-                "skinUrl": textures.skin_url,
-                "capeUrl": textures.cape_url,
-            }
-        })))
+        Ok(AuroraResponse::ok(ProfileResponseDto {
+            username: user.login,
+            is_alex: textures.is_alex,
+            skin_url: textures.skin_url,
+            cape_url: textures.cape_url,
+        }))
     }
 
     pub async fn profiles(
         db: &DatabaseConnection,
         body: aurora::dto::RequestProfilesDto,
-    ) -> Result<Json<Value>, BackendError> {
+    ) -> Result<Json<AuroraResponse<Vec<ProfilesResponseDto>>>, BackendError> {
         let users = DatabaseService::find_users(db, users::Column::Login, body.usernames)
             .await
             .map_err(|_| BackendError::BadRequestAurora("Users not found".into()))?;
 
-        let resposne: Vec<Value> = users
+        let resposne: Vec<ProfilesResponseDto> = users
             .into_iter()
-            .map(|user| {
-                json!({
-                        "id": user.uuid,
-                        "name": user.login,
-                })
+            .map(|user| ProfilesResponseDto {
+                id: user.id.to_string(),
+                name: user.login,
             })
             .collect();
 
-        Ok(Json(json!({"success": true, "result": resposne})))
+        Ok(AuroraResponse::ok(resposne))
+    }
+}
+
+impl<T> AuroraResponse<T> {
+    pub fn ok(result: T) -> Json<Self> {
+        Json(AuroraResponse {
+            success: true,
+            result,
+        })
     }
 }
